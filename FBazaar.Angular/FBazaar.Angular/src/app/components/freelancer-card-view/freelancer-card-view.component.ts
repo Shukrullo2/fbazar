499819f97f8b91dbb824ed6122a81a99b164d1d5
Malika Temurova <Malika.Temurova@ventionteams.com>
2024-10-14 14:40:36 +0500

    frontend-mt:setup thee basic structure of the angular project. Created reusable components and pages based on the markup provided

---
+++ FBazaar.Angular/FBazaar.Angular/src/app/components/freelancer-card-view/freelancer-card-view.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-freelancer-card-view',
+  standalone: true,
+  imports: [],
+  templateUrl: './freelancer-card-view.component.html',
+  styleUrl: './freelancer-card-view.component.scss'
+})
+export class FreelancerCardViewComponent {
+
+}
