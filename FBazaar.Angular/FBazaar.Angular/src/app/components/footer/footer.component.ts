e0d3ffe4d429a5d7d34597de1457d0ae1465e0d3
Malika Temurova <Malika.Temurova@ventionteams.com>
2024-10-18 17:40:07 +0500

    frontend-mt: added styles and account pages markups

--- FBazaar.Angular/FBazaar.Angular/src/app/components/footer/footer.component.ts
+++ FBazaar.Angular/FBazaar.Angular/src/app/components/footer/footer.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [],
+  imports: [RouterLink, RouterLinkActive],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss'
 })
 export class FooterComponent {
 
 }
+
